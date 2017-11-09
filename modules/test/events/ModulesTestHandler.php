ee6ddd047e9b2de80abcb713ad2e7f1975b2c43a
sushaowei <sushaowei@chehang168.com>
2017-11-09 17:27:11 +0800

    xxx

--- modules/test/events/ModulesTestHandler.php
+++ modules/test/events/ModulesTestHandler.php
@@ -1,12 +1,16 @@
 <?php
 namespace app\modules\test\events;
 use see\event\BeforeAction;
+use see\event\EventException;
 class ModulesTestHandler extends \see\base\Object
 {
 	public function init(){
 		$eventHandler = \See::$app->getEventHandler('TestHandler');
 		BeforeAction::off([$eventHandler,'testHandler']);
-		// BeforeAction::on([$this,'sign']);
+		BeforeAction::on([$this,'sign']);
+		EventException::on(function(){
+			echo "event exception\n";
+		});
 	}
 
 	public function sign($event){
